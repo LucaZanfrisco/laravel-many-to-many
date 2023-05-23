a48db705e649134d7ee070e469d3e7584e726c3e
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-23 16:10:37 +0200

    Migrazione tabella pivot

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -18,8 +18,8 @@ class Project extends Model
     public function type(){
         return $this->belongsTo(Type::class);
     }
-    
+
     public function technologies(){
-        return $this->belongsToMany(Thechnology::class);
+        return $this->belongsToMany(Thechnology::class)->withTimestamps();
     }
 }
